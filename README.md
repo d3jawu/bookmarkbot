2047247886da7bb77450e32d01e8ebbbef1d4800
Madison Swain-Bowden <bowdenm@spu.edu>
2024-12-10 16:45:35 -0800

    Add multi-tenancy (#12)
    * Move main logic to Host class, update config for multitenancy
    
    * Minor fixes regarding imports and mounting scripts
    
    * Add note for README

--- README.md
+++ README.md
@@ -11,6 +11,16 @@ A bot for managing bookmarks on Matrix/Element.
    2. Enter it into the `config.json`
 4. Run the bot: `just up`
 5. Ensure the session is verified
-   1. Open the "Sessions" settings in the Element App
-   2. Verify the session for the bot (it will show up with a red warning and be a series of letters in all caps)
-   3. Restart the bot
+   1. If there was a previous key, remove the `crypto_store` directory for that host
+   2. Open the "Sessions" settings in the Element App
+   3. Verify the session for the bot (it will show up with a red warning and be a series of letters in all caps)
+   4. Restart the bot
+
+## Dump
+Previous storage info:
+
+```json
+  "STORAGE_PATH": "./store/bookmarks.json",
+  "MATRIX_STORAGE_PATH": "./store/.matrix_storage.json",
+  "CRYPTO_STORE_PATH": "./store/.crypto_store"
+```
--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import { default as config } from "../config.json" with { type: "json" };
 
-import {
-  MatrixClient,
-  RustSdkCryptoStorageProvider,
-  SimpleFsStorageProvider,
-} from "matrix-bot-sdk";
-
-import { Storage } from "./Storage.js";
+import { Host } from "./Host.js";
 
 import { default as configTemplate } from "../config.template.json" with { type: "json" };
+import { join as joinPath } from "path";
 
 // sigh...
 /** @typedef {Record<string, any>} Event */
@@ -19,209 +14,17 @@ Object.keys(configTemplate).forEach((key) => {
   }
 });
 
-const {
-  HOMESERVER_URL,
-  ACCESS_TOKEN,
-  BOOKMARK_STORAGE_PATH,
-  MATRIX_STORAGE_PATH,
-  CRYPTO_STORE_PATH,
-} = config;
+// Settings that are consistent across users
+const { HOSTS } = config;
+const STORE_PATH = joinPath(process.cwd(), "store");
 
-const ACTIVE_ROOMS = config.ACTIVE_ROOMS.split(",");
-
-const CHECKMARKS = ["☑️", "✔️", "✅️", "✅"];
-
-const storage = new Storage(BOOKMARK_STORAGE_PATH);
-
-const client = new MatrixClient(
-  HOMESERVER_URL,
-  ACCESS_TOKEN,
-  new SimpleFsStorageProvider(MATRIX_STORAGE_PATH),
-  new RustSdkCryptoStorageProvider(CRYPTO_STORE_PATH)
-);
-
-client.on(
-  "room.event",
-  /**
-   *
-   * @param {string} roomId
-   * @param {Event} event
-   */
-  async (roomId, event) => {
-    try {
-      if (!ACTIVE_ROOMS.includes(roomId)) {
-        return;
-      }
-
-      // Create bookmark with message
-      if (
-        event.type === "m.room.message" &&
-        event?.content?.body?.startsWith("🔖")
-      ) {
-        createBookmark(
-          roomId,
-          event?.event_id,
-          event.content.body.replace("🔖", "").trim()
-        );
-      }
-
-      // Create bookmark with reaction
-      if (
-        event.type === "m.reaction" &&
-        event?.content?.["m.relates_to"]?.key === "🔖"
-      ) {
-        const originalEventId = event?.content?.["m.relates_to"]?.event_id;
-        /** @type {string} */
-        const excerpt = await (async () => {
-          try {
-            /** @type {Event} */
-            const originalEvent = await client.getEvent(
-              roomId,
-              originalEventId
-            );
-            console.log(originalEvent);
-            return originalEvent?.content?.body;
-          } catch (e) {
-            console.log(
-              `Warning: couldn't get original event ${originalEventId}`
-            );
-            console.log(e);
-            return "";
-          }
-        })();
-
-        createBookmark(roomId, originalEventId, excerpt || "");
-      }
-
-      // Clear bookmark by reaction
-      if (
-        event.type === "m.reaction" &&
-        // The two identical-looking checkmarks are actually different!
-        // The second one contains a variation selector: https://en.wikipedia.org/wiki/Variation_Selectors_(Unicode_block)
-        // If you step over it with the arrow keys, you'll notice it's two characters wide.
-        CHECKMARKS.includes(event?.content?.["m.relates_to"]?.key)
-      ) {
-        console.log(
-          `Clearing bookmark by reaction: ${roomId}:${event?.content?.["m.relates_to"]?.event_id}`
-        );
-        storage.clear(roomId, event?.content?.["m.relates_to"]?.event_id);
-        listBookmarks(roomId);
-      }
-
-      // Clear bookmark by message
-      if (
-        event.type === "m.room.message" &&
-        CHECKMARKS.includes(event?.content?.body?.[0])
-      ) {
-        /** @type {string} */
-        const body = CHECKMARKS.reduce(
-          (body, checkmark) => body.replace(checkmark, ""),
-          event?.content?.body || ""
-        );
-
-        const bookmarks = storage.list(roomId);
-
-
-        body
-          .split(",")
-          .map((iStr) => iStr.replace(/[^0-9]/g, ""))
-          .map((iStr) => parseInt(iStr))
-          .map((index) => {
-            if (
-              !index ||
-              isNaN(index) ||
-              index < 1 ||
-              index > bookmarks.length
-            ) {
-              client.sendEvent(roomId, "m.reaction", {
-                "m.relates_to": {
-                  rel_type: "m.annotation",
-                  event_id: event?.event_id,
-                  key: "❌️",
-                },
-              });
-              throw new Error(`Invalid bookmark index to clear: ${index}`);
-            }
-
-            return index;
-          })
-          .forEach((index) => {
-            console.log(`Clearing bookmark by message: ${roomId}; #${index}`);
-            storage.clear(roomId, bookmarks[index - 1]?.event_id || "");
-          });
-
-        client.sendEvent(roomId, "m.reaction", {
-          "m.relates_to": {
-            rel_type: "m.annotation",
-            event_id: event?.event_id,
-            key: "🆗",
-          },
-        });
-        listBookmarks(roomId);
-      }
-
-      // List bookmarks
-      if (event.type === "m.room.message" && event?.content?.body?.startsWith("📑")) {
-        listBookmarks(roomId);
-      }
-    } catch (e) {
-      console.log(e);
-    }
+HOSTS.forEach((hostConfig) => {
+  const host = new Host(hostConfig, STORE_PATH);
+  try {
+    host.start();
+  } catch (e) {
+    console.log(`Client for host ${hostConfig.HOST_PATH} encountered error:`);
+    console.log(e);
+    process.exit(1);
   }
-);
-
-/**
- * @param {string} roomId
- * @param {string} eventId
- * @returns {string}
- */
-const messageUrl = (roomId, eventId) =>
-  `https://matrix.to/#/${roomId}/${eventId}`;
-
-/**
- *
- * @param {string} roomId
- * @param {string} eventId
- * @param {string} excerpt
- */
-const createBookmark = async (roomId, eventId, excerpt) => {
-  console.log(`Creating bookmark: ${roomId}:${eventId} - ${excerpt}`);
-  storage.add(roomId, eventId, { excerpt });
-
-  client.sendEvent(roomId, "m.reaction", {
-    "m.relates_to": {
-      rel_type: "m.annotation",
-      event_id: eventId,
-      key: "🆗",
-    },
-  });
-};
-
-/**
- * @param {string} roomId
- */
-function listBookmarks(roomId) {
-  const bookmarks = storage.list(roomId);
-  console.log(`Listing bookmarks:`);
-  console.log(bookmarks);
-  client.sendHtmlText(
-    roomId,
-    bookmarks.length !== 0
-      ? `<b>📚️ Current bookmarks 📚️</b><br/><ol>
-        ${bookmarks
-          .map(
-            ({ excerpt, event_id }) =>
-              `<li>${excerpt} ${messageUrl(roomId, event_id)}</li>`
-          )
-          .join("\n")}
-        </ol>`
-      : `There are no bookmarks! :3`
-  );
-}
-
-try {
-  client.start();
-} catch (e) {
-  console.log("Client encountered error:");
-  console.log(e);
-}
+});
